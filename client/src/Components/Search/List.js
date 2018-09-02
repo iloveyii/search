00dadbd6ceb2f32a5a1aa9cf4d31e8e89d60d391
Hazrat Ali <ali@triggybet.com>
2018-09-02 20:08:38 +0200

    Fixed url for list

--- client/src/Components/Search/List.js
+++ client/src/Components/Search/List.js
@@ -44,7 +44,11 @@ export class List extends React.Component {
                         <aside style={{"padding": "0"}} className="col-sm-10 offset-sm-1 text-center">
                             <div className="card">
                                 <article className="card-body">
-                                    <h4 className="card-title text-center mb-4 mt-1">{site[0].title} </h4>
+                                    <h4 className="card-title text-center mb-4 mt-1">
+                                        <a href={site[0].url}>
+                                            {site[0].title}
+                                        </a>
+                                        </h4>
                                     <hr/>
                                     {
                                         site[0].attributes.map((item, i)=><Item key={i} item={item}/>)
